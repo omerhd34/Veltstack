d6b24b064e5688df3ecaea2e97ef776912dd880e
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-07-13 01:06:02 +0300

    Sayfa meta Türkçe başlıklarında blog ifadesi makaleler olarak güncellendi.

--- messages/pages/meta/tr.ts
+++ messages/pages/meta/tr.ts
@@ -3,9 +3,9 @@ export const pagesTr = {
   servicesSubtitle: "Web, mobil ve SEO'da uçtan uca dijital çözümler.",
   projectsTitle: "Projelerimiz",
   projectsSubtitle: "Dört seçilmiş proje ve detaylı proje incelemeleri.",
-  blogTitle: "Blog",
-  blogSubtitle: "Blog yazıları yakında burada listelenecek.",
+  blogTitle: "Makaleler",
+  blogSubtitle: "Makaleler yakında burada listelenecek.",
   serviceDetailSuffix: "Hizmet detay sayfası | Veltstack",
   projectDetailSuffix: "Proje detay sayfası | Veltstack",
-  blogDetailSuffix: "Blog yazısı | Veltstack",
+  blogDetailSuffix: "Makale | Veltstack",
 } as const;
