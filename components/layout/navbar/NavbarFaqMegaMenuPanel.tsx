732a5b0835ce258780c25f50e4cf10cfb25fe706
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-07-13 20:06:09 +0300

    Navbar SSS mega menü paneli eklendi.

---
+++ components/layout/navbar/NavbarFaqMegaMenuPanel.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useLocale, useTranslations } from "next-intl";
+import { LuCircleHelp } from "react-icons/lu";
+import { getNavFaqItems } from "@/components/pages/faq/faq-data";
+import { useUiStore } from "@/store/uiSlice";
+import { NavbarMegaMenuItem } from "./NavbarMegaMenuItem";
+import { NavbarMegaMenuShell } from "./NavbarMegaMenuShell";
+
+export function NavbarFaqMegaMenuPanel() {
+  const locale = useLocale() as "tr" | "en";
+  const tNav = useTranslations("nav");
+  const setFaqMenuOpen = useUiStore((state) => state.setFaqMenuOpen);
+  const items = getNavFaqItems(locale);
+
+  const closeMenu = () => setFaqMenuOpen(false);
+
+  return (
+    <NavbarMegaMenuShell
+      allHref="/sss"
+      allLabel={tNav("faqAll")}
+      onNavigate={closeMenu}
+    >
+      {items.map((item) => (
+        <NavbarMegaMenuItem
+          key={item.slug}
+          href={`/sss#${item.slug}`}
+          title={item.question}
+          icon={LuCircleHelp}
+          onNavigate={closeMenu}
+        />
+      ))}
+    </NavbarMegaMenuShell>
+  );
+}
