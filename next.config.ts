d52ae9476b1a4757aabeee5e2eaf7e2a1a4e1507
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-07-13 21:01:29 +0300

    Yönlendirmedeki gereksiz kurallar kaldırıldı.

--- next.config.ts
+++ next.config.ts
@@ -27,66 +27,6 @@ const nextConfig: NextConfig = {
         destination: "/en/hizmetler/uygulama-gelistirme",
         permanent: true,
       },
-      {
-        source: "/hizmetler",
-        destination: "/hizmetler",
-        permanent: true,
-      },
-      {
-        source: "/hizmetler/:path*",
-        destination: "/hizmetler/:path*",
-        permanent: true,
-      },
-      {
-        source: "/en/hizmetler",
-        destination: "/en/hizmetler",
-        permanent: true,
-      },
-      {
-        source: "/en/hizmetler/:path*",
-        destination: "/en/hizmetler/:path*",
-        permanent: true,
-      },
-      {
-        source: "/projeler",
-        destination: "/projeler",
-        permanent: true,
-      },
-      {
-        source: "/projeler/:path*",
-        destination: "/projeler/:path*",
-        permanent: true,
-      },
-      {
-        source: "/en/projeler",
-        destination: "/en/projeler",
-        permanent: true,
-      },
-      {
-        source: "/en/projeler/:path*",
-        destination: "/en/projeler/:path*",
-        permanent: true,
-      },
-      {
-        source: "/hizmetler/web-sitesi",
-        destination: "/hizmetler/web-sitesi-gelistirme",
-        permanent: true,
-      },
-      {
-        source: "/en/hizmetler/web-sitesi",
-        destination: "/en/hizmetler/web-sitesi-gelistirme",
-        permanent: true,
-      },
-      {
-        source: "/hizmetler/uygulama",
-        destination: "/hizmetler/uygulama-gelistirme",
-        permanent: true,
-      },
-      {
-        source: "/en/hizmetler/uygulama",
-        destination: "/en/hizmetler/uygulama-gelistirme",
-        permanent: true,
-      },
     ];
   },
 };
