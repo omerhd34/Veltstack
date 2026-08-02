10bc748438ac21e8040e006905998c44310dbcec
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-08-02 22:02:14 +0300

    Kullanılmayan ServicesPageHeroImage dosyası silindi.

--- components/pages/services/ServicesPageHeroImage.tsx
+++
@@ -1,5 +0,0 @@
-export {
-  PageHeroImage as ServicesPageHeroImage,
-  PAGE_HERO_IMAGE_FRAME_CLASS,
-  PAGE_HERO_IMAGE_WRAPPER_CLASS,
-} from "@/components/ui/page-hero";
--- components/pages/services/index.ts
+++ components/pages/services/index.ts
@@ -1,16 +1,11 @@
-export { ServicesPageHero } from "./ServicesPageHero"
-export {
-  ServicesPageHeroImage,
-  PAGE_HERO_IMAGE_FRAME_CLASS,
-  PAGE_HERO_IMAGE_WRAPPER_CLASS,
-} from "./ServicesPageHeroImage";
-export { ServicesPackagesSection } from "./ServicesPackagesSection"
-export { ServicesPackagesPanel } from "./ServicesPackagesPanel"
-export { ServicesPackagesIntro } from "./ServicesPackagesIntro"
-export { ServicesCategoryTabs } from "./ServicesCategoryTabs"
-export { ServicesPackageTypeTabs } from "./ServicesPackageTypeTabs"
-export { ServicePackageCard } from "./ServicePackageCard"
-export { ServicePackageComparison } from "./ServicePackageComparison"
-export { ServicesPageCTA } from "./ServicesPageCTA"
-export { ServicesConsultationCTA } from "./ServicesConsultationCTA"
-export { ServicesPageView } from "./ServicesPageView"
+export { ServicesPageHero } from "./ServicesPageHero";
+export { ServicesPackagesSection } from "./ServicesPackagesSection";
+export { ServicesPackagesPanel } from "./ServicesPackagesPanel";
+export { ServicesPackagesIntro } from "./ServicesPackagesIntro";
+export { ServicesCategoryTabs } from "./ServicesCategoryTabs";
+export { ServicesPackageTypeTabs } from "./ServicesPackageTypeTabs";
+export { ServicePackageCard } from "./ServicePackageCard";
+export { ServicePackageComparison } from "./ServicePackageComparison";
+export { ServicesPageCTA } from "./ServicesPageCTA";
+export { ServicesConsultationCTA } from "./ServicesConsultationCTA";
+export { ServicesPageView } from "./ServicesPageView";
