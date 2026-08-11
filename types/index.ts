b943cf9a9aafff1cd0cde5ac87352c4a312bd52c
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-08-12 02:57:28 +0300

    Type tanımları dosyaları kaldırıldı.

--- types/index.ts
+++
@@ -1,4 +0,0 @@
-export type { Project, ProjectCategory } from "./project"
-export type { Service } from "./service"
-export type { Post } from "./post"
-export type { Testimonial } from "./testimonial"
