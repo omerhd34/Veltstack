dd852f9d6083448bbf3a1c4a3cbcfddb8ad6ed37
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-08-17 22:41:21 +0300

    Otomatik dil algılama kapatıldı.

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -4,6 +4,7 @@ export const routing = defineRouting({
   locales: ["tr", "en"],
   defaultLocale: "tr",
   localePrefix: "as-needed",
+  localeDetection: false,
 })
 
 export type Locale = (typeof routing.locales)[number]
