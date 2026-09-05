15c592895a9182fbb9437d2288de3a958d19c129
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-09-06 02:00:22 +0300

    Locale loading görünümü sadeleştirildi

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -1,8 +1,3 @@
-import { getTranslations } from "next-intl/server";
-import { LoadingView } from "@/components/pages/status";
-
-export default async function LocaleLoading() {
-  const t = await getTranslations("status.loading");
-
-  return <LoadingView label={t("label")} ariaLabel={t("aria")} />;
+export default function LocaleLoading() {
+  return <div className="flex-1" aria-hidden />;
 }
