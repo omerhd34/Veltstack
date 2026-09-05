03a71d760de72f0c2e189eecb19758637ecb1f91
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-09-06 01:59:52 +0300

    Meta Pixel kullanıcı etkileşimi sonrası yüklenecek şekilde güncellendi

--- components/layout/MetaPixel.tsx
+++ components/layout/MetaPixel.tsx
@@ -1,20 +1,77 @@
 "use client";
 
-import Script from "next/script";
 import { usePathname } from "next/navigation";
 import { useEffect, useRef } from "react";
 
 const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID;
 
+type FbqFn = ((...args: unknown[]) => void) & {
+  callMethod?: (...args: unknown[]) => void;
+  queue: unknown[][];
+  loaded: boolean;
+  version: string;
+  push: FbqFn;
+};
+
 declare global {
   interface Window {
-    fbq?: (...args: unknown[]) => void;
+    fbq?: FbqFn;
+    _fbq?: FbqFn;
   }
+}
+
+function loadMetaPixel(pixelId: string) {
+  if (window.fbq) return;
+
+  const fbq = ((...args: unknown[]) => {
+    if (fbq.callMethod) {
+      fbq.callMethod(...args);
+      return;
+    }
+    fbq.queue.push(args);
+  }) as FbqFn;
+
+  fbq.queue = [];
+  fbq.loaded = true;
+  fbq.version = "2.0";
+  fbq.push = fbq;
+  window.fbq = fbq;
+  window._fbq = fbq;
+
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = "https://connect.facebook.net/en_US/fbevents.js";
+  document.head.appendChild(script);
+
+  fbq("init", pixelId);
+  fbq("track", "PageView");
 }
 
 export function MetaPixel() {
   const pathname = usePathname();
+  const loadedRef = useRef(false);
   const isInitialLoad = useRef(true);
+
+  useEffect(() => {
+    if (!META_PIXEL_ID) return;
+
+    const enable = () => {
+      if (loadedRef.current || !META_PIXEL_ID) return;
+      loadedRef.current = true;
+      loadMetaPixel(META_PIXEL_ID);
+    };
+
+    const gestures = ["pointerdown", "keydown", "touchstart"] as const;
+    for (const event of gestures) {
+      window.addEventListener(event, enable, { once: true, passive: true });
+    }
+
+    return () => {
+      for (const event of gestures) {
+        window.removeEventListener(event, enable);
+      }
+    };
+  }, []);
 
   useEffect(() => {
     if (!META_PIXEL_ID) return;
@@ -28,35 +85,15 @@ export function MetaPixel() {
   if (!META_PIXEL_ID) return null;
 
   return (
-    <>
-      <Script
-        id="meta-pixel"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${META_PIXEL_ID}');
-            fbq('track', 'PageView');
-          `,
-        }}
+    <noscript>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        height={1}
+        width={1}
+        style={{ display: "none" }}
+        src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+        alt=""
       />
-      <noscript>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          height={1}
-          width={1}
-          style={{ display: "none" }}
-          src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
-          alt=""
-        />
-      </noscript>
-    </>
+    </noscript>
   );
 }
