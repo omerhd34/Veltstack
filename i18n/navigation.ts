752c855cdbf198258644012be5d8c9dda59f542f
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-09-08 23:52:27 +0300

    i18n Link varsayılan prefetch kapatıldı

--- i18n/navigation.ts
+++ i18n/navigation.ts
@@ -1,5 +1,11 @@
-import { createNavigation } from "next-intl/navigation"
-import { routing } from "./routing"
+import { createElement, type ComponentProps } from "react";
+import { createNavigation } from "next-intl/navigation";
+import { routing } from "./routing";
 
-export const { Link, redirect, usePathname, useRouter, getPathname } =
-  createNavigation(routing)
+const navigation = createNavigation(routing);
+
+export const { redirect, usePathname, useRouter, getPathname } = navigation;
+
+export function Link(props: ComponentProps<typeof navigation.Link>) {
+  return createElement(navigation.Link, { ...props, prefetch: false });
+}
